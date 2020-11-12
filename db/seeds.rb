5c164fc7556973d9f3f0e2ec45a7e7930515eaa8
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-12 12:47:13 +0000

    Dose model migration and seeded ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts "Clearing Ingredients Database"
+Ingredient.destroy_all
+
+puts "Creating ingredients!"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+serialized = open(url).read
+ingredients_hash = JSON.parse(serialized)
+
+ingredients = ingredients_hash["drinks"]
+ingredients.each do |hash|
+  puts "Ingredient Created"
+  Ingredient.create(name: "#{hash["strIngredient1"]}")
+end
